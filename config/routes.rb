dd746703782e4fdd77f8dfcab9eb7ec1a60d195e
Koji <kohzih@gmail.com>
2022-07-30 15:35:01 +0900

    chapter 4-5 finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  # Session
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # User
   namespace :admin do
     resources :users
   end
+
+  # Task
   root to: 'tasks#index'
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
